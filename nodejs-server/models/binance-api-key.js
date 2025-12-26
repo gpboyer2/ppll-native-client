21626e4f780225922a856278eff0670df4e72f71
Raymond <773561801@qq.com>
2025-12-26 16:07:35 +0800

    refactor: 修改binance_api_keys模型允许user_id为空

--- nodejs-server/models/binance-api-key.js
+++ nodejs-server/models/binance-api-key.js
@@ -26,7 +26,7 @@ module.exports = (sequelize, DataTypes) => {
   binance_api_keys.init({
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       comment: '关联的用户ID'
     },
     name: {
